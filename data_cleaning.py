b19a62b9b21f7c03ec71ca04d7b02023c3721554
Ansar485 <ansar.serkesh@nu.edu.kz>
2024-10-07 19:34:17 +0500

    Plotted with plotly, looks fines. Next step: optimize the word2vec algorithm, sentiment scores are mostly distributed near 0.

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,11 +1,15 @@
-import ast
+import numpy as np
 import pandas as pd
 from gensim.models import Word2Vec
 from my_functions import cleaning, document_vector, cosine_sim
 import multiprocess
+from sklearn.preprocessing import MinMaxScaler
+import ast
 #import nltk
 #from nltk.corpus import opinion_lexicon
+pd.set_option('display.max_columns', None)
 
+"""
 # Reading the datasets
 trump = pd.read_csv('data/hashtag_donaldtrump.csv',
                     lineterminator='\n',
@@ -14,16 +18,16 @@ biden = pd.read_csv('data/hashtag_joebiden.csv',
                     lineterminator='\n',
                     usecols = ['created_at', 'tweet', 'country','state_code'])
 
-
 # Data cleaning
 trump = cleaning(trump, candidate = 'Trump')
 biden = cleaning(biden, candidate = 'Biden')
 data = pd.concat([trump, biden], ignore_index = True) # concatenating the dataframes
+"""
 
 # If you want to save the results before proceeding
-"""data.to_csv('data_cleaned.csv', encoding='utf-8', header=True)
+"""data.to_csv('data_cleaned.csv', encoding='utf-8', header=True)"""
 data = pd.read_csv('data_cleaned.csv')
-data['clean_text'] = data['clean_text'].apply(ast.literal_eval)"""
+data['clean_text'] = data['clean_text'].apply(ast.literal_eval)
 
 model = Word2Vec(data['clean_text'],
                  vector_size = 100,
@@ -57,29 +61,51 @@ positivity_vector = positive_vector - negative_vector
 # Calculating the cosine similarity between positivity vector and each word vector of a tweet
 data['sentiment_score'] = data['vector'].apply(lambda x: cosine_sim(x, positivity_vector))
 
-#data['sentiment_score'] = pd.to_numeric(data['sentiment_score']) # converting to numeric data type
+# Normalizing sentiment scores
+scaler = MinMaxScaler(feature_range = (-1, 1))
+data['sentiment_score'] = scaler.fit_transform(data[['sentiment_score']])
 
 # Calculating the mean of sentiment scores for each date, candidate, state group
 data = data.groupby(['created_at', 'candidate', 'state_code'])['sentiment_score'].mean().reset_index()
 
-
 # Pivot the data to have one column per candidate
-pivoted_data = (data.pivot_table(
+data = (data.pivot_table(
     index = ['created_at', 'state_code'],
     columns = 'candidate',
     values = 'sentiment_score')
-)
+).reset_index()
 
 # Reading actual results
 actual_results = pd.read_csv('data/actual_results_by_state.csv',
                              usecols = ['state_abr', 'state', 'trump_win', 'biden_win'])
 
-# Merging the actual results with the above created dataframe
+# Merging with the actual results
 data = data.merge(actual_results, left_on = 'state_code', right_on = 'state_abr', how = 'left')
 data = data[['created_at', 'state_code', 'Biden', 'Trump', 'state', 'trump_win', 'biden_win']]
 
-# Creating a dataframe for overall picture (not States based mean sentiment scores for both candidates over time)
-data_general = data.copy().groupby('created_at').agg({'Biden': "mean", 'Trump': 'mean'}).reset_index()
+# Sentiment Scores Over Time
+data_general = data.copy()
+data_general['created_at'] = pd.to_datetime(data_general['created_at'])
+data_general = data_general.groupby(data_general['created_at'].dt.date).agg({'Biden': "mean", 'Trump': 'mean'}).reset_index()
+
+# Sentiment Scores by States
+data_by_states = data.copy().groupby('state_code').agg({'Biden': 'mean', 'Trump': 'mean'}).reset_index()
+data_by_states['won_predicted'] = np.where(data_by_states['Trump'] > data_by_states['Biden'], 'Trump', 'Biden')
+
+data_by_states = data_by_states.merge(actual_results,
+                                      left_on = 'state_code',
+                                      right_on = 'state_abr',
+                                      how = 'left')
+data_by_states['won_actual'] = np.where(data_by_states['trump_win'] == 1, 'Trump', 'Biden')
+data_by_states['prediction'] = np.where(
+    data_by_states['won_predicted'] == data_by_states['won_actual'], 'Correct', 'Incorrect')
+data_by_states = data_by_states[
+    ['state_code', 'Biden', 'Trump', 'state', 'won_predicted','won_actual', 'prediction']
+]
+
 # Saving the data
 data.to_csv('data_sentiments.csv', encoding='utf-8', header=True)
-data.to_csv('data_general.csv', encoding='utf-8', header=True)
+data_general.to_csv('data_general.csv', encoding='utf-8', header=True, index = False)
+data_by_states.to_csv('data_by_states.csv', encoding='utf-8', header=True, index = False)
+
+
--- plotting.py
+++ plotting.py
@@ -1,31 +1,137 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.express as px
+import plotly
+from pandas import read_csv
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import seaborn as sns
+import os
+#pd.set_option('display.max_columns', None)
+
 # Reading the data
-data = pd.read_csv('data_sentiments.csv', parse_dates = ['created_at'])
-data['created_at'] = data['created_at'].dt.date
-actual_results = pd.read_csv('data/actual_results_by_state.csv',
-                             usecols = ['state_abr', 'state', 'trump_win', 'biden_win'])
-data = data.merge(actual_results, left_on = 'state_code', right_on = 'state_abr', how = 'left')
-data = data[['created_at', 'state_code', 'Biden', 'Trump', 'state', 'trump_win', 'biden_win']]
-pd.set_option('display.max_columns', None)
+data_sentiments = pd.read_csv('data_sentiments.csv', parse_dates = ['created_at'])
+data_sentiments['created_at'] = data_sentiments['created_at'].dt.date
 
-data_general = data.copy().groupby('created_at').agg({'Biden': "mean", 'Trump': 'mean'}).reset_index()
-plt.figure(figsize=(10, 6))
+data_general = read_csv('data_general.csv', parse_dates = ['created_at'])
+data_general['created_at'] = data_general['created_at'].dt.date
 
-# Plot sentiment scores for each candidate over time
-for candidate in data_general.columns:
-    if candidate in ['Trump', 'Biden']:
-        plt.plot(data_general['created_at'], data_general[candidate], label = candidate)
+data_by_states = read_csv('data_by_states.csv')
 
-# Step 4: Customize the plot
+#Plotting density plots for Trump and Biden
+fig, ax = plt.subplots(1, 1, figsize=(12, 6))
+fig.suptitle("Density Plot of Sentiment Scores")
+
+# Plot density for Trump
+sns.kdeplot(data_sentiments.Trump, ax = ax, alpha = 0.2, fill = True, color = 'red', label = 'Trump')
+
+# Plot density for Biden
+sns.kdeplot(data_sentiments.Biden, ax = ax, alpha = 0.2, fill = True, color = 'blue', label = 'Biden')
+
+# Plotting labels and legends
+ax.set_xlabel('Sentiment Score')
+ax.set_ylabel('Density')
+ax.legend()
+
+plt.tight_layout()
+
+# Creating folder 'images' to save all the images into
+if not os.path.exists("images"):
+    os.makedirs("images")
+
+plt.savefig('images/Density Plots.png')
+plt.clf()
+
+#Plotting sentiment scores by candidates over time
+plt.figure(figsize = (10, 6))
+
+# Plotting sentiment scores for each candidate over time
+plt.plot(data_general['created_at'], data_general['Biden'], label = 'Biden', color = 'blue')
+plt.plot(data_general['created_at'], data_general['Trump'], label = 'Trump', color = 'red')
+# Customizing the plot
 plt.title('Sentiment Scores Over Time by Candidate')
 plt.xlabel('Date')
 plt.ylabel('Sentiment Score')
-plt.legend(title='Candidate')
+plt.legend(title = 'Candidate')
 plt.grid(True)
-plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+plt.xticks(rotation = 45)  # rotating x-axis labels for better readability
 
-# Show the plot
+# Showing the plot
 plt.tight_layout()
-plt.show()
+plt.savefig('images/Sentiment Scores by Candidate Over Time.png')
+
+# Plotting Sentiment Scores by States for Trump and Biden
+fig_trump = px.choropleth(data_by_states,
+                    locations = 'state_code',
+                    locationmode = 'USA-states',
+                    scope = 'usa',
+                    color = 'Trump', labels = {'Trump': 'Sentiment score'},
+                    range_color = (-1, 1),
+                    hover_name = 'state_code',
+                    hover_data = ['Trump'],
+                    color_continuous_scale = 'reds',
+                    title = 'US 2020 Elections: Trump Sentiment Scores by State'
+)
+
+plotly.offline.plot(fig_trump,
+                    filename = 'images/Sentiment Scores by State, Trump.html')
+
+fig_biden = px.choropleth(data_by_states,
+                    locations = 'state_code',
+                    locationmode = 'USA-states',
+                    scope = 'usa',
+                    color = 'Biden', labels = {'Biden': 'Sentiment score'},
+                    range_color = (-1, 1),
+                    hover_name = 'state_code',
+                    hover_data = ['Biden'],
+                    color_continuous_scale = 'blues',
+                    title = 'US 2020 Elections: Biden Sentiment Scores by State'
+)
+
+plotly.offline.plot(fig_biden,
+                    filename = 'images/Sentiment Scores by State, Biden.html')
+
+# Checking the Predicted Winner and the Actual Winner by States
+fig_predicted = px.choropleth(data_by_states,
+                              locations='state_code',
+                              locationmode='USA-states',
+                              scope='usa',
+                              color='won_predicted',  # Color by predicted winner
+                              hover_name='state',
+                              hover_data=['won_predicted'],
+                              color_discrete_map={'Biden': 'blue', 'Trump': 'red'},
+                              title='US 2020 Elections: Predicted Winner by State')
+
+plotly.offline.plot(fig_predicted, filename = 'images/Predicted Winner by State.html')
+
+fig_actual = px.choropleth(data_by_states,
+                           locations='state_code',
+                           locationmode='USA-states',
+                           scope='usa',
+                           color='won_actual',  # Color by actual winner
+                           hover_name='state',
+                           hover_data=['won_actual'],
+                           color_discrete_map={'Biden': 'blue', 'Trump': 'red'},
+                           title='US 2020 Elections: Actual Winner by State')
+
+plotly.offline.plot(fig_actual, filename = 'images/Actual Winner by State.html')
+
+# Predictions results
+fig_results = px.choropleth(data_by_states,
+                            locations='state_code',
+                            locationmode='USA-states',
+                            scope='usa',
+                            color='prediction',
+                            hover_name='state',
+                            hover_data=['prediction'],
+                            color_discrete_map={'Correct': 'green', 'Incorrect': 'red'},
+                            title='US 2020 Elections: Correct Predictions by State')
+
+plotly.offline.plot(fig_results, filename = 'images/Prediction Results by State.html')
+
+# Calculating Error Rate
+total = len(data_by_states['prediction_correct'])
+incorrect = total - data_by_states['prediction_correct'].sum()
+err_rate = incorrect/total
+print(f'The error rate is: {round(err_rate * 100, 2)}%')
+
