c4158b6031f063961e2c56d84b2c2d92d6616ed9
Ansar485 <ansar.serkesh@nu.edu.kz>
2024-11-01 19:55:49 +0500

    Some summary statistics of the cleaned data

--- summary_statistics.py
+++ summary_statistics.py
@@ -2,6 +2,12 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from pandas.plotting import table
+import os
+
+folder_path = 'summary_statistics'
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+
 
 pd.set_option('display.max_columns', None)
 data = pd.read_csv('data_cleaned.csv', parse_dates = ['created_at'])
@@ -23,14 +29,15 @@ sns.displot(data = data, x = 'state_code', hue = 'candidate', kde = True, aspect
 plt.xlabel('State Code')
 plt.title('Distribution of Tweets by State and Candidate')
 plt.show()
-plt.savefig('distr_of_tweets_by_state_and_candidate.png')
+
+plt.savefig(folder_path + '/distr_of_tweets_by_state_and_candidate.png')
 
 # Plot 2
 sns.displot(data = data, x = 'created_at', hue = 'candidate', kde = True, aspect = 3)
 plt.xlabel('Date')
 plt.title('Distribution of Tweets by Date and Candidate')
 plt.show()
-plt.savefig('distr_of_tweets_by_date_and_candidate.png')
+plt.savefig(folder_path + '/distr_of_tweets_by_date_and_candidate.png')
 
 
 
